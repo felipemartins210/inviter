033abec8d11cce98a389ee55c7dcfc154a93ce0c
Felipe Martins <felipemartins210@gmail.com>
2021-08-10 10:43:48 -0300

    :wrench: Firebase DB config

---
+++ src/config/firebase.js
@@ -0,0 +1,20 @@
+import firebase from "firebase";
+import 'firebase/storage';
+
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyC_hD-wunkgW0RR9cD4GG6GAEIcFqn0GsY",
+    authDomain: "invite-8cf82.firebaseapp.com",
+    projectId: "invite-8cf82",
+    storageBucket: "invite-8cf82.appspot.com",
+    messagingSenderId: "23648446675",
+    appId: "1:23648446675:web:062a2414f4d0d1dc24cc26"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  const database = firebase.firestore()
+  export default database
+
+
+   
