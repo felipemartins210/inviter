e838871d68d1718135aac1a44d335589477db4ad
Felipe Martins <felipemartins210@gmail.com>
2021-08-26 15:59:45 -0300

    :construction: Base pages
    minimal estructure page

---
+++ src/pages/NewUser/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function NewUser() {
+  return (
+    <View>
+      <Text>New User</Text>
+    </View>
+  );
+}
