31c2b9f09a0f2e2fcade6ac958b6036cfe0bd826
Felipe Martins <felipemartins210@gmail.com>
2021-08-26 16:03:01 -0300

    :construction: conts and function login

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,10 +1,30 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  KeyboardAvoidingView,
+} from "react-native";
+
+import firebase from "../../config/firebase";
+import styles from "./style";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorLogin, setErrorLogin] = useState("");
+
+  const loginFirebase = () => {
+    //TODO > login firebase
+  };
+
   return (
     <View>
       <Text>Login</Text>
     </View>
   );
 }
+
+//! rnf - export default function React Snp.
